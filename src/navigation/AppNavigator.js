25e5441d95d21b6dc618cc4b64ef0ea7a2feffc6
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2025-12-27 10:52:35 +0530

    updated navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,23 +1,26 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import DashboardScreen from '../screens/DashboardScreen';
-// import BillingScreen from '../screens/BillingScreen';
-// import StockScreen from '../screens/StockScreen';
-// import CustomersScreen from '../screens/CustomersScreen';
-
+import DashboardScreen from "./screens/DashboardScreen";
+import StockScreen from "./screens/StockScreen";
+import BillingScreen from "./screens/BillingScreen";
+import CustomerPendingScreen from "./screens/CustomerPendingScreen";
+import DealerScreen from "./screens/DealerScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-      
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Dashboard" component={DashboardScreen} />
-        {/* <Stack.Screen name="Billing" component={BillingScreen} />
         <Stack.Screen name="Stock" component={StockScreen} />
-        <Stack.Screen name="Customers" component={CustomersScreen} /> */}
+        <Stack.Screen name="Billing" component={BillingScreen} />
+        <Stack.Screen
+          name="CustomerPending"
+          component={CustomerPendingScreen}
+        />
+        <Stack.Screen name="Dealer" component={DealerScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
