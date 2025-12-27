f8781970f7b4584f49341bafef0bec3267009d54
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2025-12-27 10:48:20 +0530

    added dashboard screen

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,70 +1,21 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from "react-native";
+import Header from "../components/Header";
+import InfoBox from "../components/InfoBox";
 
 export default function DashboardScreen() {
-
-  const totalSalesToday = 12450;
-  const pendingCustomers = 5;
-  const lowStockItems = 3;
-
   return (
     <View style={styles.container}>
+      <Header title="Dashboard" />
 
-      <Text style={styles.title}>Retail Business Dashboard</Text>
-
-      <View style={styles.box}>
-        <Text style={styles.boxTitle}>Today's Sales</Text>
-        <Text style={styles.boxValue}>₹ {totalSalesToday}</Text>
-      </View>
-
-
-      <View style={styles.box}>
-        <Text style={styles.boxTitle}>Pending Customers</Text>
-        <Text style={styles.boxValue}>{pendingCustomers}</Text>
-      </View>
-
-      <View style={styles.box}>
-        <Text style={styles.boxTitle}>Low Stock Items</Text>
-        <Text style={styles.boxValue}>{lowStockItems}</Text>
-      </View>
-
+      <InfoBox label="Total Customers" value="15" />
+      <InfoBox label="Pending Amount" value="₹6200" />
+      <InfoBox label="Low Stock Items" value="4" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#e6e6e6',
-    padding: 15
+    padding: 10,
   },
-  
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 15,
-    color: '#333'
-  },
-
-  box: {
-    backgroundColor: '#d1d1d1',
-    padding: 12,
-    marginBottom: 12,
-
-    shadowColor: '#000',
-    shadowOffset: { width: 5, height: 5 },
-    shadowOpacity: 0.9,
-    shadowRadius: 2,
-    elevation: 6
-  },
-  boxTitle: {
-    fontSize: 14,
-    color: '#555'
-  },
-  boxValue: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#111',
-    marginTop: 5
-  }
 });
