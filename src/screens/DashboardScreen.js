c468658ebf27cd105b82c1d9c7532cc273c422c6
yashdeepdeshmukh18 <ybdeshmukh123@gmail.com>
2025-12-27 11:01:31 +0530

    added navigation buttons on dashboard

--- src/screens/DashboardScreen.js
+++ src/screens/DashboardScreen.js
@@ -1,15 +1,37 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Button } from "react-native";
 import Header from "../components/Header";
 import InfoBox from "../components/InfoBox";
 
-export default function DashboardScreen() {
+export default function DashboardScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Header title="Dashboard" />
 
-      <InfoBox label="Total Customers" value="15" />
-      <InfoBox label="Pending Amount" value="₹6200" />
-      <InfoBox label="Low Stock Items" value="4" />
+      <InfoBox label="Total Customers" value="10" />
+      <InfoBox label="Pending Amount" value="₹5200" />
+      <InfoBox label="Low Stock Items" value="2" />
+
+      <View style={styles.btn}>
+        <Button title="Go to Stock" onPress={() => navigation.navigate("Stock")} />
+      </View>
+
+      <View style={styles.btn}>
+        <Button title="Create Bill" onPress={() => navigation.navigate("Billing")} />
+      </View>
+
+      <View style={styles.btn}>
+        <Button
+          title="Customer Pending"
+          onPress={() => navigation.navigate("CustomerPending")}
+        />
+      </View>
+
+      <View style={styles.btn}>
+        <Button
+          title="Dealer Payments"
+          onPress={() => navigation.navigate("Dealer")}
+        />
+      </View>
     </View>
   );
 }
@@ -17,5 +39,8 @@ export default function DashboardScreen() {
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+  },
+  btn: {
+    marginVertical: 5,
   },
 });
